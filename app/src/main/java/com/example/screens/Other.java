b79508421541bab55c5f911b06007e63f160fb61
b16oskni <b16oskni@student.his.se>
2020-04-28 19:32:27 +0200

    Added second activity Added fragment to second activity

---
+++ app/src/main/java/com/example/screens/Other.java
@@ -0,0 +1,16 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class Other extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+
+    }
+}
